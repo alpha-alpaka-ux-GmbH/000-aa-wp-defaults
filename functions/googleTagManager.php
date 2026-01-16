ef2eed20fd94322ee80ef62e19d3c90c21bdfa70
Nic <66076642+NicAA1@users.noreply.github.com>
2026-01-16 13:11:21 +0100

    fixed googleTagManager and userCentrics were integrated twice

--- functions/googleTagManager.php
+++ functions/googleTagManager.php
@@ -4,156 +4,61 @@ namespace AlphaAlpaka\Defaults;
 
 use function Env\env;
 
-/**
- * Prevent UC / usercentrics on Datenschutz or Impressum Page
- */
-
-add_action(
-    'init',
-    function () {
-        if (env('WP_ENV') === 'production' && !\is_user_logged_in()) :
-            if ((env('UC_SETTINGS_ID') && env('UC_SETTINGS_ID') !== '')):
-                add_action('wp_head', function () {
-?>
-                <link rel='dns-prefetch' href='https://privacy-proxy.usercentrics.eu' />
-                <link rel='preconnect' href='//app.usercentrics.eu' />
-                <link rel='preconnect' href='//api.usercentrics.eu' />
-                <link rel='preconnect' href='//privacy-proxy.usercentrics.eu' />
-                <link rel='preload' href='//app.usercentrics.eu/browser-ui/latest/loader.js' as='script' />
-                <link rel='preload' href='//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js' as='script' />
-                <script id='usercentrics-cmp' data-settings-id="<?php echo env('UC_SETTINGS_ID'); ?>" src='https://app.usercentrics.eu/browser-ui/latest/loader.js' data-tcf-enabled></script>
-                <script type='application/javascript' src='https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js'></script>
-            <?php
-                }, 1);
-            ?>
-
-
-            <?php
-                add_action('wp_footer', function () {
-            ?>
-                <script type="application/javascript">
-                    document.addEventListener('DOMContentLoaded', async () => {
-
-                        /**
-                         * UC Banner
-                         */
-
-                        var elementsWithin = document.querySelectorAll('.js__trigger_uc a');
-                        var elementsWithClass = document.querySelectorAll('a.js__trigger_uc');
-                        var elementsWithHash = document.querySelectorAll('a[href*="#js__trigger_uc"]');
-
-                        var ucTriggers = Array.from(elementsWithin).concat(Array.from(elementsWithClass)).concat(Array.from(elementsWithHash));
-
-                        if (ucTriggers) {
-                            ucTriggers.forEach(function(element) {
-                                element.addEventListener('click', function(e) {
-                                    // console.log('trigger uc-banner')
-                                    e.preventDefault();
-
-                                    if (typeof UC_UI !== 'undefined' && UC_UI) {
-                                        UC_UI.showSecondLayer()
-                                    } else {
-                                        // console.log('UC Banner not loaded')
-                                    }
-                                });
-                            });
-                        }
-                    });
-                </script>
-                <style>
-                    .uc-embed {
-                        .uc-checkbox {
-                            input {
-                                display: inline-block;
-                                padding: 32px;
-                            }
-                        }
-
-                        div[data-testid="uc-embed-service"] {
-                            padding: 32px;
-
-                            &:first-of-type {
-                                margin-top: 0;
-                            }
-                        }
-
-
-                        h4 {
-                            padding: 32px;
-                        }
-
-                        .uc-embed-subelement {
-                            padding: 32px;
-                        }
-                    }
-                </style>
-                <?php
-                    if (is_page()) :
-                        $page = get_queried_object();
-                        if ($page->post_title == 'Datenschutz' || $page->post_title == 'Impressum') :
-                ?>
-                        <script type="application/javascript">
-                            var UC_UI_SUPPRESS_CMP_DISPLAY = true;
-                        </script>
-                <?php
-                        endif;
-                    endif;
-                }, 100);
-            endif;
-
-
-            if ((env('GTM_PROPERTY_ID') && env('GTM_PROPERTY_ID') !== '')):
-                add_action('wp_head', function () {
-                ?>
-                <script>
-                    // create dataLayer
-                    window.dataLayer = window.dataLayer || [];
-
-                    function gtag() {
-                        dataLayer.push(arguments);
-                    }
-
-                    // set „denied" as default for both ad and analytics storage,
-                    gtag("consent", "default", {
-                        ad_user_data: "denied",
-                        ad_personalization: "denied",
-                        ad_storage: "denied",
-                        analytics_storage: "denied",
-                        wait_for_update: 2000 // milliseconds to wait for update
-                    });
-
-                    // Enable ads data redaction by default [optional]
-                    gtag("set", "ads_data_redaction", true);
-
-                    // Google Tag Manager
-                    (function(w, d, s, l, i) {
-                        w[l] = w[l] || [];
-                        w[l].push({
-                            'gtm.start': new Date().getTime(),
-                            event: 'gtm.js'
-                        });
-                        var f = d.getElementsByTagName(s)[0],
-                            j = d.createElement(s),
-                            dl = l != 'dataLayer' ? '&l=' + l : '';
-                        j.async = true;
-                        j.src =
-                            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                        f.parentNode.insertBefore(j, f);
-                    })(window, document, 'script', 'dataLayer', "<?php echo env('GTM_PROPERTY_ID'); ?>");
-                </script>
-            <?php
-                }, 1);
-
-                add_action('wp_body_open', function () {
-            ?>
-                <!-- Google Tag Manager (noscript) -->
-                <noscript>
-                    <iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo env('GTM_PROPERTY_ID'); ?>" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-                </noscript>
-                <!-- End Google Tag Manager (noscript) -->
-<?php
-                }, 1);
-            endif;
-        endif;
+function aa_init_google_tag_manager()
+{
+    // Check if GTM ID is set and valid
+    $gtm_id = env('GTM_PROPERTY_ID');
+    if (!$gtm_id || $gtm_id === '') {
+        return;
     }
-);
+
+    // Scripts enqueuen
+    add_action('wp_enqueue_scripts', function () use ($gtm_id) {
+        // Dummy Script registrieren, um Inline-Script anzuhängen
+        // Dies erlaubt uns, wp_add_inline_script zu nutzen
+        wp_register_script('aa-gtm', false);
+        wp_enqueue_script('aa-gtm');
+
+        $gtm_id_esc = esc_js($gtm_id);
+
+        $script = <<<JS
+            // create dataLayer
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+
+            // set "denied" as default for both ad and analytics storage
+            gtag("consent", "default", {
+                ad_user_data: "denied",
+                ad_personalization: "denied",
+                ad_storage: "denied",
+                analytics_storage: "denied",
+                wait_for_update: 2000
+            });
+
+            // Enable ads data redaction by default [optional]
+            gtag("set", "ads_data_redaction", true);
+
+            // Google Tag Manager
+            (function(w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({
+                    'gtm.start': new Date().getTime(),
+                    event: 'gtm.js'
+                });
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s),
+                    dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', "{$gtm_id_esc}");
+JS;
+
+        wp_add_inline_script('aa-gtm', $script);
+    });
+}
+add_action('template_redirect', __NAMESPACE__ . '\\aa_init_google_tag_manager');
--- functions/userCentrics.php
+++ functions/userCentrics.php
@@ -2,14 +2,16 @@
 
 namespace AlphaAlpaka\Defaults;
 
-function aa_is_privacy_or_impressum_page()
+use function Env\env;
+
+function is_privacy_or_impressum_page()
 {
     if (!is_page()) return false;
     $page = get_queried_object();
     return $page && isset($page->post_title) && in_array($page->post_title, ['Datenschutz', 'Impressum'], true);
 }
 
-function aa_enqueue_usercentrics_assets()
+function aa_init_usercentrics()
 {
     if (env('WP_ENV') !== 'production' || is_user_logged_in() || empty(env('UC_SETTINGS_ID'))) return;
 
@@ -23,68 +25,47 @@ function aa_enqueue_usercentrics_assets()
         echo "<link rel='preload' href='//privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js' as='script' />\n";
     }, 1);
 
-    // Usercentrics Scripts
-    wp_enqueue_script(
-        'usercentrics-cmp',
-        'https://app.usercentrics.eu/browser-ui/latest/loader.js',
-        [],
-        null,
-        true
-    );
-    wp_enqueue_script(
-        'usercentrics-block',
-        'https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js',
-        [],
-        null,
-        true
-    );
+    // Scripts enqueuen
+    add_action('wp_enqueue_scripts', function () {
+        // Usercentrics Scripts
+        wp_enqueue_script(
+            'usercentrics-cmp',
+            'https://app.usercentrics.eu/browser-ui/latest/loader.js',
+            [],
+            null,
+            true
+        );
 
-    // Eigene Banner-Logik
-    wp_enqueue_script(
-        'usercentrics-banner',
-        plugin_dir_url(__FILE__) . 'assets/js/usercentrics-banner.js',
-        [],
-        null,
-        true
-    );
+        wp_enqueue_script(
+            'usercentrics-block',
+            'https://privacy-proxy.usercentrics.eu/latest/uc-block.bundle.js',
+            [],
+            null,
+            true
+        );
 
-    // Eigene Styles
-    wp_enqueue_style(
-        'usercentrics-banner',
-        plugin_dir_url(__FILE__) . 'assets/css/usercentrics-banner.css',
-        [],
-        null
-    );
+        // Eigene Banner-Logik (u.a. Link um Banner zu öffnen)
+        wp_enqueue_script(
+            'usercentrics-banner',
+            plugin_dir_url(dirname(__DIR__) . '/index.php') . 'aa-wp-defaults/assets/js/usercentrics-banner.js',
+            [],
+            null,
+            true
+        );
 
-    // Dynamische Werte als Inline-Script
-    $settings_id = esc_js(env('UC_SETTINGS_ID'));
-    $suppress = aa_is_privacy_or_impressum_page() ? 'true' : 'false';
-    $inline = "window.UC_SETTINGS_ID = '{$settings_id}'; window.UC_UI_SUPPRESS_CMP_DISPLAY = {$suppress};";
-    wp_add_inline_script('usercentrics-banner', $inline);
+        // Eigene Styles (u.a. für embedds auf der Datenschutz-Seite)
+        wp_enqueue_style(
+            'usercentrics-banner',
+            plugin_dir_url(dirname(__DIR__) . '/index.php') . 'aa-wp-defaults/assets/css/usercentrics-banner.css',
+            [],
+            null
+        );
 
-    // GTM nur wenn vorhanden
-    $gtm_id = env('GTM_PROPERTY_ID');
-    if (!empty($gtm_id)) {
-        $gtm_js = "window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('consent', 'default', {
-            ad_user_data: 'denied',
-            ad_personalization: 'denied',
-            ad_storage: 'denied',
-            analytics_storage: 'denied',
-            wait_for_update: 2000
-        });
-        gtag('set', 'ads_data_redaction', true);
-        (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', '{$gtm_id}');";
-        wp_add_inline_script('usercentrics-banner', $gtm_js);
-    }
+        // Dynamische Werte als Inline-Script
+        $settings_id = esc_js(env('UC_SETTINGS_ID'));
+        $suppress = is_privacy_or_impressum_page() ? 'true' : 'false';
+        $inline = "window.UC_SETTINGS_ID = '{$settings_id}'; window.UC_UI_SUPPRESS_CMP_DISPLAY = {$suppress};";
+        wp_add_inline_script('usercentrics-banner', $inline);
+    });
 }
-add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\aa_enqueue_usercentrics_assets');
+add_action('template_redirect', __NAMESPACE__ . '\\aa_init_usercentrics');
